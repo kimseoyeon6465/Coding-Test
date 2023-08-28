b0d7edddf9b04d8df23141d6e2d4e7e4728df401
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-08-28 19:31:48 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> dot) {
+    int answer = 0;
+    if (dot[0]<0)
+        if(dot[1]<0)
+            answer=3;
+        else
+            answer=2;
+    else
+        if(dot[1]<0)
+            answer=4;
+        else
+            answer=1;
+    return answer;
+}
