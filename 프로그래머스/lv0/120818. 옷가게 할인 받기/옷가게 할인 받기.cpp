5887296355eea3e2404dc3c3c443c29fedab7406
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-08-30 19:03:24 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -2,16 +2,18 @@
 #include <vector>
 
 using namespace std;
-/*
+
 
 int solution(int price) {
     int answer = 0;
-    if(price>=100000 && price<300000) answer=(int)price*0.95;
+    if(price<100000) answer=price;
+    else if(price>=100000 && price<300000) answer=(int)price*0.95;
     else if(price>=300000 && price<500000) answer=(int)price*0.9;
     else if(price>=500000) answer=(int)price*0.8;
     return answer;
 }
-*/
+
+/*
 int solution(int price) {
     int answer = price;
     if (price >= 500000) {
@@ -23,3 +25,4 @@ int solution(int price) {
     }
     return answer;
 }
+*/
