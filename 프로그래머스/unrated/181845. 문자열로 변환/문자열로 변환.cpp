343e8158b4a6bd6dc375a61d04fa47f13adfff5d
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-09-11 22:38:46 +0900

    [unrated] Title: 문자열로 변환, Time: 0.02 ms, Memory: 3.83 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    answer= to_string(n);
+    return answer;
+}
