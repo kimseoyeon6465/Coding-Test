d1e112007f4e8158af36e9082ed913c2c44ce163
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-10-15 23:21:16 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 28.20 ms, Memory: 27.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int k) {
+    vector<int> answer(arr.begin(),arr.end());
+    for(int i =0; i<arr.size();i++)
+    {
+        if(k%2==0)
+        {
+            answer[i]=arr[i]+k;
+        }
+        else
+        {
+            answer[i]=arr[i]*k;
+        }
+    }
+    
+    return answer;
+}
