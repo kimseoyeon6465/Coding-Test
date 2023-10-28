b062ced9cca0dbdf5de4eed5596a1d285555bb9f
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-10-28 23:40:15 +0900

    [level unrated] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.02 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> strArr) {
+    vector<string> answer=strArr;
+    for(int i=0;i<answer.size();i++)
+    {
+        if(i%2==0)
+        {
+            for(int j=0;j<answer[i].length();j++)
+                answer[i][j]=tolower(answer[i][j]);
+        }
+        else{
+            for(int j=0;j <answer[i].length();j++)
+            {
+                answer[i][j]=toupper(answer[i][j]);
+            }
+        }
+    }
+    return answer;
+}
