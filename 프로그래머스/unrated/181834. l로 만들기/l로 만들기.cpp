edae614a4b137d72973e7527d38a3101eb2b0c9a
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2023-11-13 23:40:30 +0900

    [level unrated] Title: l로 만들기, Time: 0.32 ms, Memory: 3.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    
+    for(char& c : myString)
+    {
+        if(c<'l')
+        {
+            c='l';
+        }
+    }
+    return myString;
+}
